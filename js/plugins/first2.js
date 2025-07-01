b4710087f4be55827c790ee84c4944b1cd030b9f
meetmarvelous <marvel1698@gmail.com>
2025-07-01 14:16:59 +0100

    second

---
+++ js/plugins/first2.js
@@ -0,0 +1,40 @@
+jQuery.fn.setiLightbox = function () {
+  var thumbnailPath = '';
+  if (jQuery('#tg_lightbox_thumbnails').val() != '') {
+    thumbnailPath = jQuery('#tg_lightbox_thumbnails').val();
+  }
+  else {
+    thumbnailPath = 'horizontal';
+  }
+
+  var iLightboxapi = jQuery('a.fancy-gallery, .pp_gallery a, .img_frame, .fancy_video, .lightbox_youtube, .woocommerce-product-gallery__image a').iLightBox({
+    skin: jQuery('#tg_lightbox_skin').val(),
+    path: thumbnailPath,
+    type: 'inline, video, image',
+    maxScale: 1,
+    controls: {
+      slideshow: true,
+      arrows: true,
+      thumbnail: jQuery('#tg_lightbox_thumbnails_display').val()
+    },
+    overlay: {
+      opacity: jQuery('#tg_lightbox_opacity').val()
+    },
+    mobile: {
+      // Enable on mobile devices
+      enabled: true,
+      // Don't prevent default touch behavior
+      preventDefaultTouch: false
+    },
+    callback: {
+      onOpen: function (api, position) {
+        jQuery('body').addClass('blur');
+      },
+      onHide: function () {
+        jQuery('body').removeClass('blur');
+      }
+    }
+  });
+
+  iLightboxapi.refresh();
+}
